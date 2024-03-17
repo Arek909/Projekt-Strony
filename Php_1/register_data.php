70db89f92d92e1aef31de54da9a51c5b3eae31d6
Arek909 <162886376+Arek909@users.noreply.github.com>
2024-03-17 18:33:38 +0100

    Update register_data.php

--- Php_1/register_data.php
+++ Php_1/register_data.php
@@ -9,6 +9,28 @@ $login = $_POST["login"];
 $haslo = $_POST["haslo"];
 $i = 0;
 
+// sprawdz czy login lub mail nie jest zajety
+$sgl_check_login = "SELECT * FROM logindb WHERE login = '$login'";
+$result_check_login = mysqli_query($conn, $sgl_check_login);
+if (mysqli_num_rows($result_check_login) > 0) {
+    // jesli login lub mail sie powtarzaja ustaw sesje z komunikatem
+    session_start();
+    $_SESSION['username_taken'] = true;
+    // Przekieruj użytkownika z powrotem do formularza rejestracji
+    header("Location: register.php");
+    exit();
+}
+// Sprawdź, czy numer prawa jazdy nie istnieje już w bazie danych
+$sql_check_nr_prawajazdy = "SELECT * FROM logindb WHERE nr_prawajazdy = '$nr_prawajazdy'";
+$result_check_nr_prawajazdy = mysqli_query($conn, $sql_check_nr_prawajazdy);
+if (mysqli_num_rows($result_check_nr_prawajazdy) > 0) {
+    // Jeśli numer prawa jazdy już istnieje, ustaw sesję z komunikatem
+    session_start();
+    $_SESSION['nr_prawajazdy_taken'] = true;
+    // Przekieruj użytkownika z powrotem do formularza rejestracji
+    header("Location: register.php");
+    exit();
+}
 
 // Znalezienie maksymalnego numeru id_user w tabeli
 $sql_max_id = "SELECT MAX(id_user) AS max_id FROM logindb";
